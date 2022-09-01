c1e181c753320f32836675a33d5dcc607f29393d
samirjabib <samir.jc123@gmail.com>
2022-09-01 13:07:18 -0500

    add checkout item styled component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,5 +1,14 @@
 import { useContext } from "react";
-import './checkout-item.styles.scss'
+
+import { 
+    Arrow,
+    BaseSpan,
+    CheckoutItemContainer,
+    ImageContainer,
+    Quantity,
+    RemoveButton,
+    Value
+} from './checkout-item.styles'
 
 import { CartContext } from "../../contexts/cart.context";
 
@@ -20,25 +29,25 @@ const CheckoutItem = ({ cartItem }) => {
 
     return (
 
-        <div className="checkout-item-container">
-            <div className='image-container'>
+        <CheckoutItemContainer>
+            <ImageContainer>
             <img src={imageUrl} alt={`${name}`} />
-            </div>
-            <span className='name'> {name} </span>
-            <span className='quantity'>
-                <div className='arrow' onClick={removeItemHandler}>
+            </ImageContainer>
+            <BaseSpan> {name} </BaseSpan>
+            <Quantity>
+                <Arrow className='arrow' onClick={removeItemHandler}>
                 &#10094;
-                </div>
-                <span className='value'>{quantity}</span>
-                <div className='arrow' onClick={addItemHandler}>
+                </Arrow>
+                <Value className='value'>{quantity}</Value>
+                <Arrow className='arrow' onClick={addItemHandler}>
                 &#10095;
-                </div>
-            </span>
-            <span className='price'> {price}</span>
-            <div className='remove-button' onClick={clearItemHandler}>
+                </Arrow>
+            </Quantity>
+            <BaseSpan className='price'> {price}</BaseSpan>
+            <RemoveButton onClick={clearItemHandler}>
                 &#10005;
-      </div>
-        </div>
+            </RemoveButton>
+        </CheckoutItemContainer>
     );
 };
 
