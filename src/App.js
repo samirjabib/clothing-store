bb1b072f29f419c97e6c6bae46b03644e32a12b9
samirjabib <samir.jc123@gmail.com>
2022-09-15 17:21:23 -0500

    add auth with saga

--- src/App.js
+++ src/App.js
@@ -8,24 +8,15 @@ import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
+  getCurrentUser,
 } from './utils/firebase/firebase.utils'
-import { setCurrentUser } from './store/user/user.action';
 
 const App = () => {
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
+    getCurrentUser().then((user) => console.log(user))
 
   }, []);
 
