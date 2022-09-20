47a92574882f1ddd0b70524653bb59e2cab71c39
samirjabib <samir.jc123@gmail.com>
2022-09-19 22:53:34 -0500

    add settings for stripe

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './store/store';
-
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 import App from './App';
 
 import { Provider } from 'react-redux'
+import { Elements } from '@stripe/react-stripe-js'
 
 
 import './index.scss';
+
 
 const rootElement = document.getElementById('root');
 
@@ -20,7 +22,9 @@ render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
